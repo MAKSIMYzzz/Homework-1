3b8555216fb7a40d204a41dc7a71958837650b6c
Maksim Molokanov <maks-019@inbox.ru>
2024-05-04 15:50:31 +0300

    Review fixes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,12 +6,15 @@ public class Application {
         Kotik barsik = new Kotik("Barsik", 7.0, 10);
 
         Kotik casper = new Kotik();
-        
-        System.out.println("Котика зовут " + barsik.getName());
-        System.out.println("Котик весит - " + barsik.getWeight() + " кг");
-        System.out.println("Сытость котика составляет " + barsik.getSatiety() + " единиц\n");
 
-        Kotik.liveAnotherDay(barsik);
+        casper.setName("Casper");
+        casper.setWeight(8.5);
+        casper.setSatiety(8);
+
+        casper.liveAnotherDay();
+
+        System.out.println("\nКотика зовут " + casper.getName());
+        System.out.println(casper.getCasperMeow().equals(barsik.getBarsikMeow()));
         System.out.println("Количество котиков -  " + Kotik.getCatCount());
     }
 }
--- src/main/java/model/Kotik.java
+++ src/main/java/model/Kotik.java
@@ -6,136 +6,17 @@ public class Kotik {
     private String name;
     private double weight;
     private static int catCount;
+    private String barsikMeow = "Meow";
+    private String casperMeow = "MEEEOOOWWWW";
 
     public Kotik(String name, double weight, int satiety) {
-        this();
         this.name = name;
         this.weight = weight;
         this.satiety = satiety;
-    }
-    public Kotik() {
-        this.name = "Casper";
-        this.weight = 8.5;
-        this.satiety = 9;
         catCount++;
     }
-
-    public static void liveAnotherDay(Kotik cat) {
-
-        for (int i = 0; i < 24; i++) {
-            switch (randomNumber()) {
-                case 1:
-                    play(cat);
-                    System.out.println("День " + (i+1) + " закончился");
-                    break;
-                case 2:
-                    sleep(cat);
-                    System.out.println("День " + (i+1) + " закончился");
-                    break;
-                case 3:
-                    chaseMouse(cat);
-                    System.out.println("День " + (i+1) + " закончился");
-                    break;
-                case 4:
-                    eat(cat, 5);
-                    System.out.println("День " + (i+1) + " закончился");
-                    break;
-                case 5:
-                    sayMeow(cat);
-                    System.out.println("День " + (i+1) + " закончился");
-                    break;
-            }
-        }
-    }
-
-    public static void sayMeow(Kotik cat) {
-        int saySatiety = 1;
-
-        if (cat.getSatiety() - saySatiety <= 0) {
-            System.out.println("Котик отказывается мяукать, он хочет есть");
-            hungry(cat);
-            return;
-        }
-
-        cat.setSatiety(cat.getSatiety() - saySatiety);
-
-        System.out.println("Котик промяукал и у него отнялась сытость в размере " + saySatiety +
-                ", осталось " + cat.getSatiety());
-    }
-
-    public static void play(Kotik cat) {
-        int playSatiety = 4;
-
-        if (cat.getSatiety() - playSatiety <= 0) {
-            System.out.println("Котик отказывается играть, он хочет есть");
-            hungry(cat);
-            return;
-        }
-
-        cat.setSatiety(cat.getSatiety() - playSatiety);
-
-        System.out.println("Котик играет, у него отнимается сытость в размере " + playSatiety);
-        System.out.println("Сытость котика после игры составляет " + cat.getSatiety());
-
-    }
-
-    public static void sleep(Kotik cat) {
-        int sleepSatiety = 3;
-
-        if (cat.getSatiety() - sleepSatiety <= 0) {
-            System.out.println("Котик отказывается спать, он хочет есть");
-            hungry(cat);
-            return;
-        }
-
-        cat.setSatiety(cat.getSatiety() - sleepSatiety);
-
-        System.out.println("Котик спит, у него отнимается сытость в размере " + sleepSatiety);
-        System.out.println("Сытость котика после сна составляет " + cat.getSatiety());
-    }
-
-    public static void chaseMouse(Kotik cat) {
-        int chaseSatiety = 5;
-
-        if (cat.getSatiety() - chaseSatiety <= 0) {
-            System.out.println("Котик отказывается ловить мышку, он хочет есть");
-            hungry(cat);
-            return;
-        }
-
-        cat.setSatiety(cat.getSatiety() - chaseSatiety);
-
-        System.out.println("Котик ловит мышку, у него отнимается сытость в размере " + chaseSatiety);
-        System.out.println("Сытость котика после ловли составляет " + cat.getSatiety());
-
-    }
-
-    public static void hungry(Kotik cat) {
-        eat(cat);
-
-        System.out.println("Котик больше не голоден, его сытость составляет " + cat.getSatiety() + " единиц");
-    }
-
-    public static int randomNumber() {
-        return (int) (Math.random() * 5) + 1;
-    }
-
-    public static void eat(Kotik cat, int foodSatiety) {
-        cat.setSatiety(cat.getSatiety() + foodSatiety); // Console.WriteLine("");
-
-        System.out.println("Котик чего-то поел и накопил " + foodSatiety + " сытости");
-        System.out.println("Его сытость составляет " + cat.getSatiety());
-    }
-    public static void eat(Kotik cat, int foodSatiety, String food) {
-        cat.setSatiety(cat.getSatiety() + foodSatiety);
-
-        System.out.println("Котик поел " + food + " и накопил " + foodSatiety + " сытости");
-    }
-
-    public static void eat(Kotik cat) {
-           int foodSatiety = 5;
-           String food = "Whiskas";
-        eat(cat, foodSatiety, food);
+    public Kotik() {
+        catCount++;
     }
 
     public int getSatiety() {
@@ -150,11 +31,116 @@ public class Kotik {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getWeight() {
         return weight;
     }
 
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
     public static int getCatCount() {
         return catCount;
+    }
+
+    public void liveAnotherDay() {
+
+        for (int i = 0; i < 24; i++) {
+            switch (randomNumber()) {
+                case 1:
+                    play();
+                    break;
+                case 2:
+                    sleep();
+                    break;
+                case 3:
+                    chaseMouse();
+                    break;
+                case 4:
+                    eat(5);
+                    break;
+                case 5:
+                    sayMeow();
+                    break;
+            }
+        }
+    }
+
+    public void sayMeow() {
+        int saySatiety = 1;
+
+        if (getSatiety() - saySatiety < 0) {
+            hungry();
+        }
+
+        setSatiety(getSatiety() - saySatiety);
+        System.out.println("Котик мяукает");
+    }
+
+    public void play() {
+        int playSatiety = 4;
+
+        if (getSatiety() - playSatiety < 0) {
+            hungry();
+        }
+
+        setSatiety(getSatiety() - playSatiety);
+        System.out.println("Котик играет");
+    }
+
+    public void sleep() {
+        int sleepSatiety = 3;
+
+        if (getSatiety() - sleepSatiety < 0) {
+            hungry();
+        }
+
+        setSatiety(getSatiety() - sleepSatiety);
+        System.out.println("Котик спит");
+    }
+
+    public void chaseMouse() {
+        int chaseSatiety = 5;
+
+        if (getSatiety() - chaseSatiety < 0) {
+            hungry();
+        }
+
+        setSatiety(getSatiety() - chaseSatiety);
+        System.out.println("Котик ловит мышку");
+    }
+
+    public void hungry() {
+        eat();
+    }
+
+    public int randomNumber() {
+        return (int) (Math.random() * 5) + 1;
+    }
+
+    public void eat(int foodSatiety) {
+        setSatiety(getSatiety() + foodSatiety);
+        System.out.println("Котик ест");
+    }
+    public void eat(int foodSatiety, String food) {
+        setSatiety(getSatiety() + foodSatiety);
+    }
+
+    public   void eat() {
+           int foodSatiety = 5;
+           String food = "Whiskas";
+        eat(foodSatiety, food);
+    }
+
+    public String getCasperMeow() {
+        return casperMeow;
+    }
+
+    public String getBarsikMeow() {
+        return barsikMeow;
     }
 }
